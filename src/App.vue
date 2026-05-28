2133e41d1cd3f2e61d222f0f01ff147e4ccf89a8
JFH000 <juanfh0000@gmail.com>
2026-05-28 18:44:50 -0500

    speed insghts fro vercel

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Analytics />
+  <SpeedInsights />
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { Analytics } from '@vercel/analytics/vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 import { useCatalogStore } from '@/modules/catalog/stores/catalog.store'
 import { useAuthStore } from '@/modules/auth/stores/auth.store'
 
