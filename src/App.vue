bfc1cacc87bc65b3f0024e9d73925141f9c64336
JFH000 <juanfh0000@gmail.com>
2026-05-28 18:41:10 -0500

    vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <Analytics />
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import { Analytics } from '@vercel/analytics/vue'
 import { useCatalogStore } from '@/modules/catalog/stores/catalog.store'
 import { useAuthStore } from '@/modules/auth/stores/auth.store'
 
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import { createPinia } from 'pinia'
 import router from './router'
 import './style.css'
 import App from './App.vue'
-
 createApp(App).use(createPinia()).use(router).mount('#app')
